8fea2369353c63b4d711579f5675a97e8b98b536
ken iiyoshi <ki573@nyu.edu>
2023-04-23 13:01:46 +0400

    slightly edited pyqt5.py again

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+#ken? date: apr 23, 2023
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
